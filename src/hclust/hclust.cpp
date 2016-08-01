fb68b0ab704e936d2d2b9410d94c6859ad4a36d7
Haifeng Chen <haifengc@usc.edu>
2016-07-31 20:07:33 -0700

    update

--- src/hclust/hclust.cpp
+++ src/hclust/hclust.cpp
@@ -16,42 +16,140 @@ struct Point {
   Point()
       : data(DIMENSION, 0.0) {
   }
+  void Output() const {
+    cout << "point";
+    for(uint32_t i = 0;i < DIMENSION;++i) {
+      cout << " " << data[i];
+    }
+    cout << endl;
+  }
   vector<double> data;
 };
+
+Point KmerToCoordinates(const string& kmer);
+
+struct KMER {
+  KMER(const string& _name, const string& _seq, const Point& _point)
+      : name(_name),
+        seq(_seq),
+        point(_point) {
+  }
+  string name;
+  string seq;
+  //Point point() const {
+    //return KmerToCoordinates(seq);
+ // }
+  Point point;
+};
+
+Point Center(const vector<KMER>& kmers, const vector<uint32_t>& ids);
+
+struct Cluster {
+  Cluster() {
+    radius = 0.0;
+  }
+
+  void AddPoint(const uint32_t& id) {
+    ids.push_back(id);
+  }
+
+  void AddCluster(const Cluster& cluster) {
+    for (uint32_t i = 0; i < cluster.ids.size(); ++i) {
+      ids.push_back(cluster.ids[i]);
+    }
+  }
+
+  Point center(const vector<KMER>& kmers) const {
+    return Center(kmers, ids);
+  }
+
+  vector<uint32_t> ids;
+  double radius;
+};
+
 
 Point KmerToCoordinates(const string& kmer) {
   Point point;
   size_t k = 0;
   for (size_t i = 0; i < kmer.size(); ++i) {
     int AA = base[kmer[i] - 'A'];
+    if(AA == -1) {
+      AA = rand() % 20;
+    }
     for (size_t j = 0; j < AACoordinateSize; ++j) {
       point.data[k++] = coordinates[AA][j];
     }
   }
+
   return point;
 }
 
-struct KMER {
-  KMER(const string& _name, const string& _seq)
-      : name(_name),
-        seq(_seq) {
-  }
-  string name;
-  string seq;
-  Point point() const {
-    return KmerToCoordinates(seq);
-  }
-};
 
 Point Center(const vector<KMER>& kmers, const vector<uint32_t>& ids) {
   Point center;
   for (uint32_t i = 0; i < ids.size(); ++i) {
     for (uint32_t j = 0; j < DIMENSION; ++j) {
-      center.data[j] += kmers[ids[i]].point().data[j];
+      center.data[j] += kmers[ids[i]].point.data[j];
     }
   }
   for (uint32_t j = 0; j < DIMENSION; ++j) {
-    center.data[j] /= DIMENSION;
+    center.data[j] /= ids.size();
+  }
+  return center;
+}
+
+Point Center(const vector<Point>& points) {
+  Point center;
+  for (uint32_t i = 0; i < points.size(); ++i) {
+    for (uint32_t j = 0; j < DIMENSION; ++j) {
+      center.data[j] += points[i].data[j];
+    }
+  }
+  for (uint32_t j = 0; j < DIMENSION; ++j) {
+    center.data[j] /= points.size();
+  }
+  return center;
+}
+
+Point Center(const vector<KMER>& kmers, const vector<Cluster>& clusters,
+             const vector<uint32_t>& ids) {
+//  Point center;
+//  for (uint32_t i = 0; i < ids.size(); ++i) {
+//    Point ci = clusters[ids[i]].center(kmers);
+//    for (uint32_t j = 0; j < DIMENSION; ++j) {
+//      center.data[j] += ci.data[j];
+//    }
+//  }
+//
+//  for (uint32_t j = 0; j < DIMENSION; ++j) {
+//    center.data[j] /= ids.size();
+//  }
+//  return center;
+
+  uint32_t size = 0;
+  for (uint32_t i = 0; i < ids.size(); ++i) {
+    size += clusters[ids[i]].ids.size();
+  }
+
+  Point center;
+  //cout << "----------center1" << endl;
+  //center.Output();
+  //cout << "----------center2" << endl;
+  for (uint32_t i = 0; i < ids.size(); ++i) {
+    for (uint32_t j = 0; j < clusters[ids[i]].ids.size(); ++j) {
+      for (uint32_t k = 0; k < DIMENSION; ++k) {
+       // cout <<  "(" << kmers[clusters[ids[i]].ids[j]].point.data[k] << "," << clusters[ids[i]].ids[j] << ") ";
+        center.data[k] += kmers[clusters[ids[i]].ids[j]].point.data[k];
+        //center.Output();
+      }
+      //cout << i << " " << j << " " << k << " " << kmers[clusters[ids[i]].ids[j]].point.data[k] << endl;
+      //cout << i << " " << j << endl;
+    }
+    //center.Output();
+  }
+ // center.Output();
+  for (uint32_t k = 0; k < DIMENSION; ++k) {
+    center.data[k] /= size;
   }
   return center;
 }
@@ -65,90 +163,148 @@ double PairwiseDistance(const Point& a, const Point& b) {
   return sqrt(dis);
 }
 
-void BuildLSHTalbe(const vector<KMER>& kmers, const LSH& lsh,
-                   vector<uint32_t>& root, HashTable& lsh_table) {
+void BuildLSHTalbe(const vector<KMER>& kmers, const vector<Cluster>& clusters,
+                   const LSH& lsh, HashTable& lsh_table) {
   cout << "BuildLSHTalbes... " << endl;
-  for (uint32_t i = 0; i < kmers.size(); ++i) {
-    if (root[i] != i)
-      continue;
-    lsh_table[lsh.HashKey(kmers[i].point().data)].push_back(i);
+  for (uint32_t i = 0; i < clusters.size(); ++i) {
+    string key = lsh.HashKey(clusters[i].center(kmers).data);
+    lsh_table[key].push_back(i);
   }
 }
+
+
+void PairwiseDistanceSampling(const vector<KMER>& kmers, const Cluster& cluster,
+                              ofstream& fout) {
+  for (int i = 0; i < cluster.ids.size(); ++i) {
+    for (int j = i + 1; j < cluster.ids.size(); ++j) {
+      fout << PairwiseDistance(kmers[cluster.ids[i]].point, kmers[cluster.ids[j]].point)
+          << endl;
+    }
+  }
+}
+
+void ClustingBucket(const vector<KMER>& kmers, const vector<Cluster>& clusters,
+                    const vector<uint32_t>& ids, const double& hash_R,
+                    vector<Cluster>& new_clusters) {
+  //if(ids.size() == 1) return;
+  Point c = Center(kmers, clusters, ids);
+  //c.Output();
+  //if(ids.size() == 1) {
+   // cout << "------------" << endl;
+   // kmers[clusters[ids[0]].ids[0]].point.Output();
+  //}
+  vector<uint8_t> label(ids.size(), 0);
+  for (uint32_t i = 0; i < ids.size(); ++i) {
+    double dis = PairwiseDistance(c, clusters[ids[i]].center(kmers));
+    //cout << "ra " << dis  << "\t" << clusters[ids[i]].radius << "\t" << dis + clusters[ids[i]].radius << "\t" << hash_R * 0.75 << endl;
+    //if (dis + clusters[ids[i]].radius > hash_R / 2) {
+    if (dis + clusters[ids[i]].radius > hash_R / 2) {
+      new_clusters.push_back(clusters[ids[i]]);
+      label[i] = 1;
+    }
+  }
+
+  Cluster cluster;
+  for (uint32_t i = 0; i < ids.size(); ++i) {
+    if (label[i] == 0) {
+      cluster.AddCluster(clusters[ids[i]]);
+    }
+  }
+
+  if (cluster.ids.size() != 0) {
+    cout << "size = " << cluster.ids.size() << endl;
+    Point center = cluster.center(kmers);
+    double max_dis = 0.0;
+    for (uint32_t i = 0; i < cluster.ids.size(); ++i) {
+      double dis = PairwiseDistance(center, kmers[cluster.ids[i]].point);
+      max_dis = max_dis > dis ? max_dis : dis;
+    }
+    cluster.radius = max_dis;
+    ///////////////////////////////////
+    for (int i = 0; i < cluster.ids.size(); ++i) {
+      for (int j = i + 1; j < cluster.ids.size(); ++j) {
+       /* cout << "dis\t" << hash_R / 2 << "\t("
+             << PairwiseDistance(c, kmers[cluster.ids[i]].point) << ", "
+             << PairwiseDistance(c, kmers[cluster.ids[j]].point) << ")\t"
+             << PairwiseDistance(kmers[cluster.ids[i]].point, kmers[cluster.ids[j]].point) << endl;*/
+      }
+    }
+    //////////////////////////////////
+    new_clusters.push_back(cluster);
+  }
+}
+
+
+
+void InnerClusterDistance(const vector<KMER>& kmers,
+                          const vector<Cluster>& clusters,
+                          const string& output_file) {
+  string out = output_file;
+  out += "inner.txt";
+  cout << out << endl;
+  ofstream fout(out.c_str());
+  for (size_t i = 0; i < clusters.size(); ++i) {
+    PairwiseDistanceSampling(kmers, clusters[i], fout);
+  }
+  fout.close();
+}
+
 
 void Clustering(const vector<KMER>& kmers, const uint32_t& hash_K,
                 const uint32_t& hash_L, const double& hash_W,
                 const double& hash_R, const string& output_file) {
-  unordered_map<uint64_t, uint32_t> count;
-  vector<uint32_t> root(kmers.size(), 0);
+  vector<Cluster> clusters(kmers.size());
   for (uint32_t i = 0; i < kmers.size(); ++i) {
-    root[i] = i;
+    clusters[i].ids.push_back(i);
+    clusters[i].radius = 0.0;
   }
-
   for (uint32_t l = 0; l < hash_L; ++l) {
+    cout << "llll = " << l << endl;
     clock_t start = clock();
     HashTable lsh_table;
     LSH lsh(DIMENSION, hash_K, hash_W);
-    BuildLSHTalbe(kmers, lsh, root, lsh_table);
-    uint64_t cnt = 0;
-    for(HashTable::iterator it = lsh_table.begin();it != lsh_table.end();++it) {
-      cnt += it->second.size();
-    }
-    cout << "num of buckets " << lsh_table.size() << endl;
-    cout << "cnt = " << cnt << endl;
+    BuildLSHTalbe(kmers, clusters, lsh, lsh_table);
     printf("Build LSH l=%d takes %lf seconds\n", l,
            (clock() - start) / (double) CLOCKS_PER_SEC);
     start = clock();
-    uint64_t sum_count = 0;
-    for (uint32_t i = 0; i < kmers.size(); ++i) {
-      if (root[i] != i) {
-        continue;
-      }
-      string key = lsh.HashKey(kmers[i].point().data);
-      HashTable::const_iterator it = lsh_table.find(key);
-      const vector<uint32_t>& ids = it->second;
-      cout << i << " " << ids.size() << endl;
-      sum_count += ids.size();
-      for (uint32_t j = 0; j < ids.size(); ++j) {
-        if (root[ids[j]] != ids[j] || ids[j] < i) {
-          continue;
-        }
-        uint64_t val;
-        if (i < ids[j]) {
-          val = i;
-          val <<= 32;
-          val += ids[j];
-        } else {
-          val = ids[j];
-          val <<= 32;
-          val += i;
-        }
-        count[val]++;
-
-        if (PairwiseDistance(kmers[i].point(), kmers[ids[j]].point())
-            <= hash_R) {
-          root[ids[j]] = i;
-        }
-      }
+    vector<Cluster> new_clusters;
+    uint32_t cnt = 0;
+    uint32_t sum_size = 0;
+    for (HashTable::const_iterator it = lsh_table.begin();
+        it != lsh_table.end(); ++it) {
+      cout << cnt++ << "\t" << lsh_table.size() << "\t" << it->second.size()
+          << endl;
+      sum_size += it->second.size();
+      ClustingBucket(kmers, clusters, it->second, hash_R, new_clusters);
     }
-    cout << "sum_count " << sum_count << endl;
+
+    cout << "sum_size " << sum_size << endl;
+    cout << "size = " << new_clusters.size() << endl;
+    //InnerClusterDistance(kmers, new_clusters, output_file);
+    clusters.clear();
+    clusters = new_clusters;
+    cout << "size = " << clusters.size() << endl;
     printf("Clustering l=%d takes %lf seconds\n", l,
            (clock() - start) / (double) CLOCKS_PER_SEC);
   }
 
-  string count_out = output_file;
-  count_out += "count.txt";
-  ofstream fsee(count_out.c_str());
-  for(unordered_map<uint64_t, uint32_t>::iterator it = count.begin();it != count.end();++it) {
-    uint32_t val1 = it->first >> 32; //higher
-    uint32_t val2 = (it->first << 32) >> 32;
-    fsee << it->first << "\t" << val1 << "\t" << val2 << "\t" << it->second << endl;
-    if(it->second > 2) cout << it->first << "\t" << val1 << "\t" << val2 << "\t" << it->second << endl;
-  }
-  fsee.close();
+//  string count_out = output_file;
+//  count_out += "count.txt";
+//  ofstream fsee(count_out.c_str());
+//  for(unordered_map<uint64_t, uint32_t>::iterator it = count.begin();it != count.end();++it) {
+//    uint32_t val1 = it->first >> 32; //higher
+//    uint32_t val2 = (it->first << 32) >> 32;
+//    fsee << it->first << "\t" << val1 << "\t" << val2 << "\t" << it->second << endl;
+//    if(it->second > 2) cout << it->first << "\t" << val1 << "\t" << val2 << "\t" << it->second << endl;
+//  }
+//  fsee.close();
 
   ofstream fout(output_file.c_str());
-  for (uint32_t i = 0; i < kmers.size(); ++i) {
-    fout << kmers[i].name << "\t" << kmers[root[i]].name << endl;
+  for (uint32_t i = 0; i < clusters.size(); ++i) {
+    fout << "#cluster" << i << endl;
+    for (uint32_t j = 0; j < clusters[i].ids.size(); ++j) {
+      fout << kmers[clusters[i].ids[j]].name << endl;
+    }
   }
   fout.close();
 }
@@ -236,7 +392,9 @@ int main(int argc, const char *argv[]) {
       if(line[0] == '>') {
         name = line;
         fin >> line;
-        kmers.push_back(KMER(name, line));
+        kmers.push_back(KMER(name, line, KmerToCoordinates(line)));
+        //cout << line << "\t";
+        //kmers.back().point.Output();
       }
     }
     fin.close();
